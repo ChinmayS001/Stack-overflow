750efb614f9e01ec665bb3a6a2369002ea2cf786
Chinmay-S <chinmay.subramanya@gmail.com>
2023-04-27 11:28:31 +0530

    third commit

--- src/Pages/ChatBot/ChatBot.jsx
+++ src/Pages/ChatBot/ChatBot.jsx
@@ -1,19 +1,73 @@
 import React, { useState } from 'react'
 import './ChatBot.css'
 import Sm from '../../assets/send-message.png';
-import { getMessage } from '../../api';
+import { Configuration, OpenAIApi } from "openai";
+//import { Configuration, OpenAIApi } from '../../api';
 const ChatBot = () => {//let chat = [];
+    
     let text = '';
      const [chat,setChat] = useState([])
      const [ques,setQuest] = useState([])
-     function updateChat(){
+  async   function updateChat(){
    
        if(text == '')return;
        //let new = chat;new.push(text);
        
-       console.log(chat);
+
+// 
+//openai.api_key = "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";
+
+// Set the parameters for the completion
+const model = 'text-davinci-002';
+const prompt = 'Hello, world!';
+const temperature = 0.5;
+//const msg = getMessage(text);
+// Generate a completion using the specified model, prompt, and temperature
+// openai.complete({
+//   engine: model,
+//   prompt: prompt,
+//   temperature: temperature,
+//   max_tokens: 1024,
+// }).then((response) => {
+//   const generated_text = response.choices[0].text.trim();
+//   console.log(generated_text);
+//        setQuest([...ques,text]);
+//        setChat([...chat,generated_text]);
+// }).catch((err) => {
+//   console.log(err);
+// });
+// console.log(msg)
+// setQuest([...ques,text]);
+// setChat([...chat,msg]);
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createCompletion({
+  model: "text-davinci-002",
+  prompt: text,
+});
+console.log(completion)
+const response = completion.data.choices[0].text;
+       console.log(response);
        setQuest([...ques,text]);
-       getMessage(text).then(response=> setChat([...chat,response]));
+       setChat([...chat,response]);
+       
+       //setChat([...chat,opp]);
+    
+
+
+
+
+
+
+
+// const response = completion.data.choices[0].text;
+//        console.log(response);
+//        setQuest([...ques,text]);
+//        setChat([...chat,response]);
        
        //setChat([...chat,opp]);
     }
